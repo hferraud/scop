291640542b5f5430dd1870f4ae2f06e0c0e1fbd9
mururoahh <ferraudhugo@gmail.com>
2024-06-23 18:55:07 +0200

    refactor engine to use vulkano crate

--- src/engine.rs
+++ src/engine.rs
@@ -1,102 +1,201 @@
-use std::collections::HashSet;
-use std::ffi::{c_char, CStr, CString};
-use std::ptr;
-use ash::{vk, Entry};
-use winit::application::ApplicationHandler;
-use winit::event::WindowEvent;
-use winit::event_loop::ActiveEventLoop;
-use winit::window::{Window, WindowId};
+use std::sync::Arc;
+use winit::{
+    window::{
+        Window,
+        WindowId,
+    },
+    event::WindowEvent,
+    event_loop::{
+        EventLoop,
+        ActiveEventLoop,
+    },
+    application::ApplicationHandler,
+};
 
-const APPLICATION_NAME: &'static str = "Scop";
-const APPLICATION_VERSION: u32 = vk::make_api_version(0, 1, 0, 0);
-const ENGINE_NAME: &'static str = "Vulkan Engine";
-const ENGINE_VERSION: u32 = vk::make_api_version(0, 1, 0, 0);
-const API_VERSION: u32 = vk::make_api_version(0, 1, 0, 0);
+use vulkano::{
+    instance::{
+        Instance,
+        InstanceCreateInfo,
+        InstanceExtensions,
+        debug::{
+            DebugUtilsMessenger,
+            DebugUtilsMessengerCallback,
+            DebugUtilsMessengerCreateInfo,
+        }
+    },
+    device::{
+        Device,
+        Queue,
+        QueueFlags,
+        physical::PhysicalDevice,
+    },
+    swapchain::Surface,
+    Version,
+    VulkanLibrary
+};
+use vulkano::device::{DeviceCreateInfo, DeviceExtensions, Features, QueueCreateInfo};
 
 const VALIDATION_LAYERS: &[&str] = &[
     "VK_LAYER_KHRONOS_validation",
 ];
 
+#[cfg(all(debug_assertions))]
+const ENABLE_VALIDATION_LAYERS: bool = true;
+#[cfg(not(debug_assertions))]
+const ENABLE_VALIDATION_LAYERS: bool = false;
+
+struct QueueFamilyIndices {
+    graphics_family: Option<u32>
+}
+
+impl QueueFamilyIndices {
+    fn new() -> Self {
+        Self {
+            graphics_family: None,
+        }
+    }
+
+    fn is_complete(&self) -> bool {
+        self.graphics_family.is_some()
+    }
+}
+
 pub struct Engine {
+    instance: Arc<Instance>,
+    surface: Arc<Surface>,
+    physical_device: Arc<PhysicalDevice>,
+    device: Arc<Device>,
+    graphics_queue: Arc<Queue>,
     window: Option<Window>,
-    entry: Entry,
-    instance: ash::Instance,
+    debug_messenger: Option<DebugUtilsMessenger>,
 }
 
 impl Engine {
-    pub fn new() -> Self {
-        let entry = Entry::linked();
-        let instance = Self::create_instance(&entry);
-
-        Engine {
-            window: None,
-            entry,
+    pub fn new(event_loop: &EventLoop<()>) -> Self {
+        let instance = Self::create_instance(&event_loop);
+        let debug_messenger = unsafe {
+            Self::debug_messenger(Arc::clone(&instance))
+        };
+        let physical_device = Self::pick_physical_device(&instance);
+        let (device, graphics_queue) = Self::create_logical_device(&instance, Arc::clone(&physical_device));
+        Self {
             instance,
+            physical_device,
+            device,
+            graphics_queue,
+            window: None,
+            debug_messenger,
         }
     }
 
-    // INSTANCE CREATION
-    fn create_instance(entry: &Entry) -> ash::Instance {
-        if cfg!(debug_assertions) && !Self::check_validation_layer_support(entry) {
-            panic!("Validation layers requested but not available");
+    fn create_instance(event_loop: &EventLoop<()>) -> Arc<Instance> {
+        let library = VulkanLibrary::new()
+            .unwrap_or_else(|e| panic!("Couldn't load Vulkan library: {:?}", e));
+        if ENABLE_VALIDATION_LAYERS && !Self::check_validation_layers(&library) {
+            panic!("Validation layers unavailable:\n{:?}", VALIDATION_LAYERS)
         }
-        let app_name = CString::new(APPLICATION_NAME).unwrap();
-        let engine_name = CString::new(APPLICATION_NAME).unwrap();
-        let app_info = vk::ApplicationInfo {
-            s_type: vk::StructureType::APPLICATION_INFO,
-            p_next: ptr::null(),
-            p_application_name: app_name.as_ptr(),
-            application_version: APPLICATION_VERSION,
-            p_engine_name: engine_name.as_ptr(),
-            engine_version: ENGINE_VERSION,
-            api_version: API_VERSION,
-            _marker: Default::default(),
-        };
-        let create_info = vk::InstanceCreateInfo {
-            s_type: vk::StructureType::INSTANCE_CREATE_INFO,
-            p_next: ptr::null(),
-            flags: vk::InstanceCreateFlags::empty(),
-            p_application_info: &app_info,
+        let extensions = Self::required_extensions(event_loop);
+        let create_info = InstanceCreateInfo{
+            application_name: Some("Scop".into()),
+            application_version: Version { major: 1, minor: 0, patch: 0 },
+            engine_name: Some("No Engine".into()),
+            engine_version: Version { major: 1, minor: 0, patch: 0 },
+            enabled_extensions: extensions,
+            enabled_layers: if ENABLE_VALIDATION_LAYERS {
+                VALIDATION_LAYERS.iter().cloned().map(String::from).collect()
+            } else {
+                Default::default()
+            },
             ..Default::default()
         };
-        unsafe {
-            entry.create_instance(&create_info, None).unwrap()
-        }
+        Instance::new(library, create_info)
+            .expect("Failed to create Vulkan instance")
     }
 
-    //VALIDATION LAYERS
-    fn vk_to_string(raw_string: &[c_char]) -> String {
-        let cstr = unsafe {
-            CStr::from_ptr(raw_string.as_ptr())
-        };
-        cstr.to_str().expect("Failed to convert vulkan raw string").to_owned()
+    fn pick_physical_device(instance: &Arc<Instance>) -> Arc<PhysicalDevice> {
+        instance.enumerate_physical_devices()
+            .expect("Failed to enumerate physical devices")
+            .find(|device| Self::check_device(device))
+            .expect("Failed to pick a physical device")
     }
 
-    fn check_validation_layer_support(entry: &Entry) -> bool {
-        let layer_properties = unsafe {
-            entry.
-            enumerate_instance_layer_properties().
-            expect("Failed to enumerate instance layers properties")
-        };
-        let available_layers: HashSet<String> = layer_properties
-            .iter()
-            .map(|layer| Self::vk_to_string(&layer.layer_name))
-            .collect();
-        println!("Instance available layers:\n{:#?}", available_layers);
-        for layer in VALIDATION_LAYERS {
-            if !available_layers.contains(&String::from(*layer)) {
-                return false
+    fn check_device(device: &PhysicalDevice) -> bool {
+        Self::find_queue_families(device).is_complete()
+    }
+
+    fn find_queue_families(device: &PhysicalDevice) -> QueueFamilyIndices {
+        let mut indices = QueueFamilyIndices::new();
+        let queue_families = device.queue_family_properties();
+        for (i, queue_family) in queue_families.iter().enumerate() {
+            if queue_family.queue_flags.contains(QueueFlags::GRAPHICS) {
+                indices.graphics_family = Some(i as u32);
+            }
+            if indices.is_complete() {
+                break;
             }
         }
-        true
+        indices
+    }
+
+    fn create_logical_device(
+        instance: &Arc<Instance>, physical_device: Arc<PhysicalDevice>
+    ) -> (Arc<Device>, Arc<Queue>) {
+        let indices = Self::find_queue_families(&physical_device);
+        let device_create_info = DeviceCreateInfo {
+            queue_create_infos: vec![QueueCreateInfo {
+                queue_family_index: indices.graphics_family.unwrap(),
+                ..Default::default()
+            }],
+            enabled_extensions: DeviceExtensions::empty(),
+            enabled_features: Features::empty(),
+            ..Default::default()
+        };
+        let (device, mut queues) = Device::new(physical_device, device_create_info)
+            .expect("Failed to create logical device");
+        let graphics_queue = queues.next()
+            .expect("Failed to create graphics queue");
+        (device, graphics_queue)
+    }
+
+    fn check_validation_layers(library: &Arc<VulkanLibrary>) -> bool {
+        let layer_properties = library.layer_properties()
+            .expect("Couldn't retrieve layers properties");
+        let available_layers: Vec<_> = layer_properties
+            .map(|layer| layer.name().to_owned())
+            .collect();
+        VALIDATION_LAYERS.iter()
+            .all(|layer| available_layers.contains(&layer.to_string()))
+    }
+
+    fn required_extensions(event_loop: &EventLoop<()>) -> InstanceExtensions {
+        let mut extensions = Surface::required_extensions(&event_loop);
+        if ENABLE_VALIDATION_LAYERS {
+            extensions.ext_debug_utils = true;
+        }
+        extensions
+    }
+
+    unsafe fn debug_messenger(instance: Arc<Instance>) -> Option<DebugUtilsMessenger> {
+        if !ENABLE_VALIDATION_LAYERS {
+            return None;
+        }
+        DebugUtilsMessenger::new(
+            instance,
+            DebugUtilsMessengerCreateInfo::user_callback(
+                DebugUtilsMessengerCallback::new(|severity, message_type, data| {
+                    println!("Debug callback: {:?}", data.message);
+                }),
+            ),
+        ).ok()
     }
 }
 
 impl ApplicationHandler for Engine {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(event_loop
+        let window = event_loop
             .create_window(Window::default_attributes())
-            .expect("Couldn't create window"));
+            .expect("Couldn't create window");
+        self.window = Some(window);
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, window_id: WindowId, event: WindowEvent) {
@@ -106,19 +205,11 @@ impl ApplicationHandler for Engine {
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
-                if let Some(w) = &self.window {
-                    w.request_redraw();
+                if let Some(window) = &self.window {
+                    window.request_redraw();
                 }
             }
             _ => (),
-        }
-    }
-}
-
-impl Drop for Engine {
-    fn drop(&mut self) {
-        unsafe {
-            self.instance.destroy_instance(None)
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,8 @@ fn main() -> Result<(), io::Error> {
     // let obj = parser::parse("resources/42.obj")?;
     // dbg!(obj);
     let event_loop = EventLoop::new().unwrap();
-    event_loop.set_control_flow(ControlFlow::Wait);
-    let mut engine = engine::Engine::new();
+    event_loop.set_control_flow(ControlFlow::Poll);
+    let mut engine = engine::Engine::new(&event_loop);
     event_loop.run_app(&mut engine).unwrap();
     Ok(())
 }
